3d1a5d656e37c2a34621ae63929163c909df32c2
MankX58 <manuelguvi500@gmail.com>
2025-04-09 07:23:57 -0500

    Added Agregar_Tarea form

---
+++ WindowsFormsApp1/views/tareas/Agregar_tarea.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp1.views.tareas
+{
+    partial class Agregar_tarea
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblTitlteTarea = new System.Windows.Forms.Label();
+            this.LblNombreTarea = new System.Windows.Forms.Label();
+            this.TxtNombreTarea = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // LblTitlteTarea
+            // 
+            this.LblTitlteTarea.AutoSize = true;
+            this.LblTitlteTarea.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTitlteTarea.Location = new System.Drawing.Point(51, 47);
+            this.LblTitlteTarea.Name = "LblTitlteTarea";
+            this.LblTitlteTarea.Size = new System.Drawing.Size(194, 25);
+            this.LblTitlteTarea.TabIndex = 0;
+            this.LblTitlteTarea.Text = "AGREGAR TAREA";
+            this.LblTitlteTarea.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LblNombreTarea
+            // 
+            this.LblNombreTarea.AutoSize = true;
+            this.LblNombreTarea.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblNombreTarea.Location = new System.Drawing.Point(53, 89);
+            this.LblNombreTarea.Name = "LblNombreTarea";
+            this.LblNombreTarea.Size = new System.Drawing.Size(52, 15);
+            this.LblNombreTarea.TabIndex = 1;
+            this.LblNombreTarea.Text = "Nombre";
+            this.LblNombreTarea.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // TxtNombreTarea
+            // 
+            this.TxtNombreTarea.Location = new System.Drawing.Point(120, 88);
+            this.TxtNombreTarea.Name = "TxtNombreTarea";
+            this.TxtNombreTarea.Size = new System.Drawing.Size(125, 20);
+            this.TxtNombreTarea.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 160);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nombre";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(120, 199);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(125, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // Agregar_tarea
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtNombreTarea);
+            this.Controls.Add(this.LblNombreTarea);
+            this.Controls.Add(this.LblTitlteTarea);
+            this.Name = "Agregar_tarea";
+            this.Text = "Agregar Tarea";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblTitlteTarea;
+        private System.Windows.Forms.Label LblNombreTarea;
+        private System.Windows.Forms.TextBox TxtNombreTarea;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
---
+++ WindowsFormsApp1/views/tareas/Agregar_tarea.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.views.tareas
+{
+    public partial class Agregar_tarea : Form
+    {
+        public Agregar_tarea()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
